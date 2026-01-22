9d3412de1e75086fbd843bf1f9acca833b2043d7
cormacoshaughnessey-maker <cormacoshaughnessey@gmail.com>
2026-01-22 14:41:39 -0500

    Begin Organization and Coding Rewind
    Created folders to organize scripts, scenes, and assets, and coded most of the rewinding functionality (although untested)

---
+++ panthers-arcade-game/Scripts/player.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+
+
+var rewind_data : Dictionary[String, Array] = {"position":[]}
+var max_rewind_length := 60
+var rewind_on_cooldown := false
+
+@onready var rewind_cooldown_timer := $RewindCooldownTimer
+
+
+ # INFO: Function run every frame/tick
+func _physics_process(delta: float) -> void:
+	save_rewind_data(delta)
+	inputs(delta)
+	# NOTE: Put any other code that needs to be run every frame/tick in a function and call it here
+
+
+ # INFO: Function that registers user input and does actions based on that
+func inputs(delta: float) -> void:
+	if Input.is_action_just_released("rewind"):
+		start_rewind_cooldown()
+	elif not rewind_on_cooldown and Input.is_action_pressed("rewind"):
+		rewind()
+	# TODO: Put conditionals for movement inputs here
+
+
+#region Rewind Functions
+ # INFO: Function that saves data to the rewind_data array
+func save_rewind_data(_delta: float) -> void:
+	rewind_data["position"].append(position)
+	if rewind_data["position"].size() > max_rewind_length:
+		rewind_data["position"].pop_front()
+	pass
+
+ # INFO: Function which moves the player 1 position back along the rewind_data array
+func rewind() -> void:
+	if rewind_data["position"].size() > 0:
+		position = rewind_data["position"].pop_back()
+	else:
+		start_rewind_cooldown()
+
+ # INFO: Begin the cooldown on rewinding, and set rewind_on_cooldown to true
+func start_rewind_cooldown() -> void:
+	rewind_on_cooldown = true
+	rewind_cooldown_timer.start()
+	# TODO: Put a cooldown for when rewind becomes usable again
+
+ # INFO: End of the cooldown on rewinding, sets rewind_on_cooldown to false
+func _finish_rewind_cooldown() -> void:
+	rewind_on_cooldown = false
+#endregion
