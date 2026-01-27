c47832f788158cdbbf4d853b628e9d5ed5895758
cormacoshaughnessey-maker <cormacoshaughnessey@gmail.com>
2026-01-27 17:11:30 -0500

    Test and Debug Rewinding
    Added basic input mapping for the directional inputs, and basic movement, in order to test rewinding, which now works

--- panthers-arcade-game/Scripts/player.gd
+++ panthers-arcade-game/Scripts/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
+var speed := 100.0
 
 var rewind_data : Dictionary[String, Array] = {"position":[]}
-var max_rewind_length := 60
+var max_rewind_length := 100
 var rewind_on_cooldown := false
+var rewinding := false
 
 @onready var rewind_cooldown_timer := $RewindCooldownTimer
 
@@ -22,25 +24,32 @@ func inputs(delta: float) -> void:
 	elif not rewind_on_cooldown and Input.is_action_pressed("rewind"):
 		rewind()
 	# TODO: Put conditionals for movement inputs here
+	# Make sure to check that rewinding is false first!
+	if not rewinding:
+		input_tests(delta)
 
 
 #region Rewind Functions
  # INFO: Function that saves data to the rewind_data array
 func save_rewind_data(_delta: float) -> void:
-	rewind_data["position"].append(position)
-	if rewind_data["position"].size() > max_rewind_length:
-		rewind_data["position"].pop_front()
+	if not rewinding:
+		rewind_data["position"].append(position)
+		if rewind_data["position"].size() > max_rewind_length:
+			rewind_data["position"].pop_front()
+	#print(rewind_data)
 	pass
 
  # INFO: Function which moves the player 1 position back along the rewind_data array
 func rewind() -> void:
 	if rewind_data["position"].size() > 0:
+		rewinding = true
 		position = rewind_data["position"].pop_back()
 	else:
 		start_rewind_cooldown()
 
  # INFO: Begin the cooldown on rewinding, and set rewind_on_cooldown to true
 func start_rewind_cooldown() -> void:
+	rewinding = false
 	rewind_on_cooldown = true
 	rewind_cooldown_timer.start()
 	# TODO: Put a cooldown for when rewind becomes usable again
@@ -48,4 +57,19 @@ func start_rewind_cooldown() -> void:
  # INFO: End of the cooldown on rewinding, sets rewind_on_cooldown to false
 func _finish_rewind_cooldown() -> void:
 	rewind_on_cooldown = false
+#endregion
+
+
+#region Debug/Testing Functions
+ # INFO: Quick functino for moving around, so that rewinding can be tested
+func input_tests(delta: float) -> void:
+	if Input.is_action_pressed("left"):
+		position.x -= speed * delta
+	if Input.is_action_pressed("right"):
+		position.x += speed * delta
+	if Input.is_action_pressed("up"):
+		position.y -= speed * delta
+	if Input.is_action_pressed("down"):
+		position.y += speed * delta
+	pass
 #endregion
