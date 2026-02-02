dd0997378c7f9534b2fc3f8768efa77e38ee7232
cormacoshaughnessey-maker <cormacoshaughnessey@gmail.com>
2026-02-02 10:39:22 -0500

    Update Rewind Attacks
    Updated rewind attacks to check for collisions with enemies; now only needs art and a function within the Enemy class to damage the enemies collided with.

--- panthers-arcade-game/Scripts/rewind_attack.gd
+++ panthers-arcade-game/Scripts/rewind_attack.gd
@@ -2,8 +2,22 @@ extends Area2D
 
 class_name RewindAttack
 
-@onready var hitbox := $CollisionShape2D
+var hitbox : CollisionShape2D
 
+
+# INFO: When the attack is spawned, disable its hitbox after a short delay, then delete it after a longer one
 func _enter_tree() -> void:
-	await get_tree().create_timer(5).timeout
+	hitbox = $CollisionShape2D
+	hitbox.disabled = false
+	await get_tree().create_timer(0.1).timeout
+	hitbox.disabled = true
+	await get_tree().create_timer(0.4).timeout
 	self.queue_free()
+
+
+# INFO: When something enters the area, check if it's an enemy, and if so, damage it
+func _on_area_entered(area: Area2D) -> void:
+	if area is Enemy:
+		#  TODO: Add a function to damage the enemy, ex. area.damage(x)
+		pass
+	pass
