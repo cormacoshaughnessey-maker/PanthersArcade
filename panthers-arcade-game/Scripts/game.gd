08718d529f8439e42d1a507176089d7b32e28afb
cormacoshaughnessey-maker <cormacoshaughnessey@gmail.com>
2026-02-10 18:04:18 -0500

    Visual Rewind Cooldown
    Gave a visual indicator to the rewind's cooldown by making the rewind bar's transparency based off of its time remaining.

--- panthers-arcade-game/Scripts/game.gd
+++ panthers-arcade-game/Scripts/game.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
-@onready var health_ui = $UI/HealthUI
-@onready var rewind_ui = $UI/RewindUI
-@onready var player = $Player
+@onready var health_ui := $UI/HealthUI
+@onready var rewind_ui := $UI/RewindUI
+@onready var player := $Player
+@onready var rewind_cooldown_timer := $Player/RewindCooldownTimer
+var rewind_cooldown_percentage := 1.0
 
  # INFO: Variable for the lives the player has remaining, with an accompanying set function which updates the UI
 var lives = 3:
@@ -16,4 +18,5 @@ func _ready():
 
 func _physics_process(delta: float) -> void:
 	rewind_ui.fill_rewind_bar(player.rewind_data_length()/2)
-	pass
+	rewind_cooldown_percentage = (1 - rewind_cooldown_timer.time_left/rewind_cooldown_timer.wait_time)
+	rewind_ui.modulate = Color(1.0, 1.0, 1.0, rewind_cooldown_percentage)
