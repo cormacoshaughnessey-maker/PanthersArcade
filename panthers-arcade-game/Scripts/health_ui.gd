5a27c8ed80714b93c2cc8d42453734a809acec21
vivannkhanna <vivanngkhanna1@gmail.com>
2026-03-02 13:57:14 -0500

    Added score UI
    About to add getting score from enemies

--- panthers-arcade-game/Scripts/health_ui.gd
+++ panthers-arcade-game/Scripts/health_ui.gd
@@ -4,7 +4,6 @@ var uilife_scene = preload("res://Scenes/ui_life.tscn")
 
 @onready var lives = $Lives
 
-
  # INFO: Sets the number of lives displayed in the UI to be equal to a new amount
 func init_lives(amount:int):
 	if lives.get_children().size() == amount:
