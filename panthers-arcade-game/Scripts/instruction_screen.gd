cdbff6b2b3fa278ff107bc4a92eafa91063ff898
cormacoshaughnessey-maker <cormacoshaughnessey@gmail.com>
2026-05-07 08:32:58 -0400

    Improved Controller Compatibility
    Improved the buttons to make them usable with the controller

--- panthers-arcade-game/Scripts/instruction_screen.gd
+++ panthers-arcade-game/Scripts/instruction_screen.gd
@@ -1,8 +1,11 @@
 extends Control
 
 
+var button_grabbed := 0
+
 func _ready() -> void:
 	$VBoxContainer/PlayButton.grab_focus()
+	button_grabbed = 0
 
 
 func _on_play_button_pressed() -> void:
@@ -11,3 +14,18 @@ func _on_play_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("down") or Input.is_action_just_pressed("up"):
+		button_grabbed+=1
+		if button_grabbed>1:
+			button_grabbed = 0
+		if button_grabbed == 1:
+			$VBoxContainer/QuitButton.grab_focus()
+		else:
+			$VBoxContainer/PlayButton.grab_focus()
+	if Input.is_action_just_pressed("rewind"):
+		if button_grabbed == 1:
+			$VBoxContainer/QuitButton.pressed.emit()
+		else:
+			$VBoxContainer/PlayButton.pressed.emit()
